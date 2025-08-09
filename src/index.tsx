7ce8509d85567383c9d838f97abaa40732dcf228
Angelica Angarita <maria.angarita.b@gmail.com>
2025-08-08 22:27:09 -0500

    correción import

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Main from './components/Main/Main'
+import { Main }  from './components/Main/Main'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
